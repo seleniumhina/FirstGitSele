1e87de171d5f0774664859cfbff6d7d278ac9c98
hinai <hinainamdar678@gmail.com>
2024-07-29 14:36:04 +0530

    Thank you allah

--- src/TestTESTNG.java
+++ src/TestTESTNG.java
@@ -8,8 +8,16 @@ public class TestTESTNG {
 		System.setProperty("webdriver.chrome.driver","C:\\\\Webdriver\\\\chromedriver-win64\\\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 		driver.get("https://macroplanfrontend.estonetech.in/app/home");
+		Thread.sleep(5000);
+	      driver.manage().window().maximize();
+	      driver.findElement(By.name("email")).sendKeys("admin@macroplan-2140.com");
+		  Thread.sleep(2000);
+		  driver.findElement(By.id("password")).sendKeys("12345678");
+		  Thread.sleep(2000);
+		  driver.findElement(By.xpath("/html/body/div[1]/div/div/div/form/div/button")).click();
+		  Thread.sleep(5000);
 
-
+ 
 	}
 
 }
